99f58707f9be6974d1b8a7639a3461ced03ad8c2
Artur Ilichev <a.lightingale@gmail.com>
2017-11-27 17:39:24 +0300

    task4 completed

---
+++ task4/solve.go
@@ -0,0 +1,47 @@
+package main
+
+import 	(
+//	"fmt"
+	"unicode"
+	"strings"
+)
+
+func RemoveEven(arr []int) (result []int) {
+	for _, value := range arr {
+		if (value % 2 != 0) {
+			result = append(result, value)
+		}
+	}
+	return;
+}
+
+func PowerGenerator(base int) func() int {
+	pow := 1;
+	return func() int {
+		pow *= base
+		return pow
+	}
+}
+
+func DifferentWordsCount(str string) (count int) {
+	str = strings.ToLower(str) + "\n"
+	words := make(map[string]bool)
+	var word string
+	for _, value := range str {
+		if unicode.IsLetter(value) {
+			word += string(value)
+		} else if word != "" {
+			if _, ok := words[word]; ok {
+				count += 1
+			} else {
+				words[word] = true
+			}
+			word = ""
+		}
+	}
+	return 
+}
+
+// func main() {
+// 	fmt.Println(DifferentWordsCount("Hello, world!HELLO  wOrlD...12"))
+// }
