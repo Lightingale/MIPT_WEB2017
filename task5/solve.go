39f14a9a4d15b1152b3d8d843db8e332612972c8
Artur Ilichev <a.lightingale@gmail.com>
2017-12-04 19:17:01 +0300

    fix

--- task5/solve.go
+++ task5/solve.go
@@ -7,9 +7,14 @@ import (
 	"strconv"
 )
 
-type Url struct {
-	Address string `json:"url"`;
+type shortUrl struct {
+	Address string `json:"key"`
 }
+
+type longUrl struct {
+	Address string `json:"url"`	
+}
+
 
 var (
 	urlStore = make(map[int]string)
@@ -17,15 +22,15 @@ var (
 )
 
 func postUrl(w http.ResponseWriter, r *http.Request) {
-	var sourceUrl Url
-	err := json.NewDecoder(r.Body).Decode(&sourceUrl)
+	var source longUrl
+	err := json.NewDecoder(r.Body).Decode(&source)
 	if err != nil {
 		panic(err)
 	}
-	urlStore[urlId] = sourceUrl.Address
-	shortUrl := Url{strconv.Itoa(urlId)}
-	urlId++
-	json.NewEncoder(w).Encode(&shortUrl)
+	urlStore[urlId] = source.Address
+	short := shortUrl{strconv.Itoa(urlId)}
+	urlId++	
+	json.NewEncoder(w).Encode(&short)
 }
 
 func getUrl(w http.ResponseWriter, r *http.Request) {
@@ -33,13 +38,15 @@ func getUrl(w http.ResponseWriter, r *http.Request) {
 	shortUrl, err := strconv.Atoi(vars["key"])
 	if err != nil {
 		panic(err)
+	}	
+	if sourceUrl, ok := urlStore[shortUrl]; ok {
+		http.Redirect(w, r, sourceUrl, 301)
 	}
-	http.Redirect(w, r, urlStore[shortUrl], 301)
 }
 
 
 func main() {
-	router := mux.NewRouter().StrictSlash(true)
+	router := mux.NewRouter()
 	router.HandleFunc("/", postUrl).Methods("POST")
 	router.HandleFunc("/{key}", getUrl)
 	http.ListenAndServe(":8082", router)
